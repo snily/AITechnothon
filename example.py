3edfd03cb73f8dfefb299fd430b41a48b0db131f
snily <snily123@gmail.com>
2023-06-28 15:48:35 +0530

    Features example.py

---
+++ example.py
@@ -0,0 +1,9 @@
+fakeUsers = [
+    { "name": "Harry Potter", "id": "user1" },
+    { "name": "Hermione Granger", "id": "user2" },
+    { "name": "Ron Weasley", "id": "user3" },
+    { "name": "Draco Malfoy", "id": "user4" },
+    { "name": "Neville Longbottom", "id": "user5" },
+    { "name": "Luna Lovegood", "id": "user6" },
+
+]
